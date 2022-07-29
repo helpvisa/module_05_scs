fb60c86576002763c0bafb3bf0b9c9e096e6ebbb
dan brackenbury <daniel.brackenbury@gmail.com>
2022-07-29 15:09:12 -0400

    added drag-n-drop and mobile support for it

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,6 +144,75 @@ $(".list-group").on("blur", "input[type='text']", function() {
 });
 
 
+// make list sortable / draggable
+$(".card .list-group").sortable({ // allows dragging these classes
+  connectWith: $(".card .list-group"), // allows dragging *to* these classes
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // temp array to store list object data
+    var tempArray = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      
+      // add this data to object
+      tempArray.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrayName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    
+    // update array on tasks object and save tasks
+    tasks[arrayName] = tempArray;
+    saveTasks();
+  }
+});
+
+
+// allow deletion of tasks by dragging and dropping over trash id
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
